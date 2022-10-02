ad0d88cc6c559bdc760d23e01479c86cddfe9e9e
Rambabu <rambabu.potula@technobraingroup.com>
2022-10-02 19:11:17 +0530

    Added Security group

--- vpc.tf
+++ vpc.tf
@@ -4,14 +4,14 @@ module "vpc" {
   name = var.VPC_NAME
   cidr = var.VpcCIDR
 
-  azs                   = [var.ZONE1, var.ZONE2, var.ZONE3]
-  private_subnets       = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
-  public_subnets        = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
+  azs             = [var.ZONE1, var.ZONE2, var.ZONE3]
+  private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
+  public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
-  enable_nat_gateway    = true
-  single_nat_gateway    = true
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Terraform   = "true"
